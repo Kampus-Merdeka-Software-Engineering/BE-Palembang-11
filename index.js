292db66ae02d275c2ffc73c7f7c5a2482d315551
jocjoc <garox.javar@gmail.com>
2023-10-06 14:04:15 +0700

    finalz

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use(TanyaRoute);
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, 'https://kampus-merdeka-software-engineering.github.io/front-end-capstone-project-section-palembang-group-11')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, 'https://kampus-merdeka-software-engineering.github.io/front-end-capstone-project-section-palembang-group-11/page/pertanyaan.html'));
 });
  
 app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`);
+    console.log(`app listening on http://localhost:${port}`);
 }); 
